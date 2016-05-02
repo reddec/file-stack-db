429e31983c4e10e6a356894bd6e7c60e7f543f0f
RedDec <net.dev@mail.ru>
2016-05-02 22:08:26 +0300

    add logging fix bugs

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ It provides:
 
 * Dynamic allocation or reusing of file stacks
 * Auto closing unused stacks that reduce file handlers usage (especially for low-cost platforms)
+
+# Dev documentation
+
+See [godoc](godoc.org/github.com/reddec/file-stack-db)
--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"flag"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strconv"
 	"strings"
@@ -33,12 +34,13 @@ func decodeHeaders(data []byte) map[string]string {
 }
 
 func pushData(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
 	data, err := ioutil.ReadAll(r.Body)
 	if err != nil {
+		log.Println("[PUSH]", "Failed read body from request for stack", vars["key"], err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	vars := mux.Vars(r)
 	headers := map[string]string{}
 	for key, value := range r.Header {
 		if strings.HasPrefix("S-", key) {
@@ -47,16 +49,20 @@ func pushData(w http.ResponseWriter, r *http.Request) {
 	}
 	stack, err := db.Find(vars["key"])
 	if err != nil {
+		log.Println("[PUSH]", "Failed find stack", vars["key"], err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	binHeaders := encodeHeaders(headers)
 
-	depth, err := stack.Push(encodeHeaders(headers), data)
+	depth, err := stack.Push(binHeaders, data)
 	if err != nil {
+		log.Println("[PUSH]", "Failed push to", vars["key"], err)
 		http.Error(w, err.Error(), http.StatusBadGateway)
 		return
 	}
 	sdepth := strconv.Itoa(depth)
+	log.Println("[PUSH]", "Pushed", len(data), "bytes with headers", len(binHeaders), "bytes to", vars["key"], "with depth-index", sdepth)
 	w.Header().Add("Id", sdepth)
 
 	w.Header().Add("Content-Type", "application/json")
@@ -68,21 +74,26 @@ func getLast(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	stack, err := db.Find(vars["key"])
 	if err != nil {
+		log.Println("[PEAK]", "Failed find stack", vars["key"], err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	headers, body, err := stack.Peak()
 	if err != nil {
+		log.Println("[PEAK]", "Failed peak stack", vars["key"], err)
 		http.Error(w, err.Error(), http.StatusBadGateway)
 		return
 	}
 	if headers == nil || body == nil {
+		log.Println("[PEAK]", "Stack", vars["key"], "is empty")
 		http.Error(w, "", http.StatusNotFound)
+		return
 	}
 	sheaders := decodeHeaders(headers)
 	for key, value := range sheaders {
 		w.Header().Add(key, value)
 	}
+	log.Println("[PEAK]", "Read stack", vars["key"], "headers:", len(headers), "bytes, body:", len(body), "bytes")
 	w.Header().Add("Count", strconv.Itoa(stack.Depth()))
 	w.WriteHeader(200)
 	w.Write(body)
@@ -92,21 +103,26 @@ func removeLast(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	stack, err := db.Find(vars["key"])
 	if err != nil {
+		log.Println("[POP]", "Failed find stack", vars["key"], err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	headers, body, err := stack.Pop()
 	if err != nil {
+		log.Println("[POP]", "Failed pop stack", vars["key"], err)
 		http.Error(w, err.Error(), http.StatusBadGateway)
 		return
 	}
 	if headers == nil || body == nil {
+		log.Println("[POP]", "Stack", vars["key"], "is empty")
 		http.Error(w, "", http.StatusNotFound)
+		return
 	}
 	sheaders := decodeHeaders(headers)
 	for key, value := range sheaders {
 		w.Header().Add(key, value)
 	}
+	log.Println("[POP]", "Read stack", vars["key"], "headers:", len(headers), "bytes, body:", len(body), "bytes")
 	w.Header().Add("Count", strconv.Itoa(stack.Depth()))
 	w.WriteHeader(200)
 	w.Write(body)
@@ -128,8 +144,9 @@ func main() {
 		panic(err)
 	}
 	router := mux.NewRouter()
-	router.Methods("GET").Path("/:key").HandlerFunc(getLast)
-	router.Methods("POST").Path("/:key").HandlerFunc(pushData)
-	router.Methods("DELete").Path("/:key").HandlerFunc(removeLast)
-	panic(http.ListenAndServe(*address, router))
+	router.Methods("GET").Path("/{key}").HandlerFunc(getLast)
+	router.Methods("POST").Path("/{key}").HandlerFunc(pushData)
+	router.Methods("DELete").Path("/{key}").HandlerFunc(removeLast)
+	http.Handle("/", router)
+	panic(http.ListenAndServe(*address, nil))
 }
